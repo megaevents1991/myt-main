83db227b5fe12c68df12a873c9a2609912fa6fe1
Yakov Khessin <qvestro@gmail.com>
2024-12-28 20:43:12 +0200

    Search

--- app/page.tsx
+++ app/page.tsx
@@ -3,25 +3,37 @@
 import Image from "next/image";
 import { events as staticEvents } from "@/lib/events-data";
 import { Combobox, Modal, useCombobox } from "@mantine/core";
-import { useState } from "react";
+import { Dispatch, RefObject, SetStateAction, useRef, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { ArrowLeftIcon } from "lucide-react";
+import { useMediaQuery } from "@mantine/hooks";
+import { Event } from "@/lib/app.types";
 
-export default function Home() {
-  // Use the staticEvents directly instead of fetching
+const SearchCombobox = ({
+  searchValue,
+  setSearchValue,
+  events,
+  ref,
+  inline,
+}: {
+  setSearchValue: Dispatch<SetStateAction<string>>;
+  events: Event[];
+  searchValue: string;
+  ref?: RefObject<HTMLInputElement>;
+  inline?: boolean;
+}) => {
+  const router = useRouter();
+
   const combobox = useCombobox();
-  const events = staticEvents;
-  const [searchVale, setSearchVale] = useState("");
-  const [showSearchModal, setShowSearchModal] = useState(false);
+
   const filteredOptions = events.filter(({ name, location }) => {
-    const value = searchVale.toLowerCase().trim();
+    const value = searchValue.toLowerCase().trim();
     return (
       name.toLowerCase().includes(value) ||
       location.name.toLowerCase().includes(value)
     );
   });
-  const router = useRouter();
 
   const options = filteredOptions.map((item) => (
     <Combobox.Option
@@ -36,56 +48,77 @@ export default function Home() {
   ));
 
   return (
-    <main>
-      <Modal
-        closeButtonProps={{
-          icon: <ArrowLeftIcon />,
-          style: { position: "absolute" },
-        }}
-        opened={showSearchModal}
-        fullScreen
-        onClose={() => setShowSearchModal(false)}
-      >
-        <Combobox
-          onOptionSubmit={(optionValue) => {
-            setSearchVale(
-              events.find((item) => item.id === optionValue)?.name || ""
-            );
-            router.push(`/order?eventId=${optionValue}`);
+    <Combobox
+      onOptionSubmit={(optionValue) => {
+        setSearchValue(
+          events.find((item) => item.id === optionValue)?.name || ""
+        );
+        router.push(`/order?eventId=${optionValue}`);
+      }}
+      store={combobox}
+    >
+      <Combobox.Target>
+        <input
+          ref={ref}
+          className={`w-full ${
+            inline ? "rounded-r " : "rounded border-secondary "
+          } p-2 text-main border`}
+          dir="rtl"
+          placeholder="חפש אירוע..."
+          value={searchValue}
+          onChange={(event) => {
+            setSearchValue(event.currentTarget.value);
+            if (event.currentTarget.value.length > 1) {
+              combobox.openDropdown();
+            } else {
+              combobox.closeDropdown();
+            }
+            combobox.updateSelectedOptionIndex();
           }}
-          store={combobox}
+          onClick={() => searchValue.length > 1 && combobox.openDropdown()}
+          onFocus={() => searchValue.length > 1 && combobox.openDropdown()}
+          onBlur={() => combobox.closeDropdown()}
+        />
+      </Combobox.Target>
+      <Combobox.Dropdown>
+        <Combobox.Options>
+          {options.length === 0 ? (
+            <Combobox.Empty>לא מצאנו משהו כזה</Combobox.Empty>
+          ) : (
+            options
+          )}
+        </Combobox.Options>
+      </Combobox.Dropdown>
+    </Combobox>
+  );
+};
+
+export default function Home() {
+  const matches = useMediaQuery("(min-width: 768px");
+  const events = staticEvents;
+  const [searchValue, setSearchValue] = useState("");
+  const [showSearchModal, setShowSearchModal] = useState(false);
+  const ref = useRef<HTMLInputElement>(null);
+
+  return (
+    <main>
+      {!matches && (
+        <Modal
+          closeButtonProps={{
+            icon: <ArrowLeftIcon />,
+            style: { position: "absolute" },
+          }}
+          opened={showSearchModal}
+          fullScreen
+          onClose={() => setShowSearchModal(false)}
         >
-          <Combobox.Target>
-            <input
-              className="w-full rounded p-2 text-main border border-secondary focus:border-main"
-              dir="rtl"
-              placeholder="חפש אירוע..."
-              value={searchVale}
-              onChange={(event) => {
-                setSearchVale(event.currentTarget.value);
-                if (event.currentTarget.value.length > 1) {
-                  combobox.openDropdown();
-                } else {
-                  combobox.closeDropdown();
-                }
-                combobox.updateSelectedOptionIndex();
-              }}
-              onClick={() => searchVale.length > 1 && combobox.openDropdown()}
-              onFocus={() => searchVale.length > 1 && combobox.openDropdown()}
-              onBlur={() => combobox.closeDropdown()}
-            />
-          </Combobox.Target>
-          <Combobox.Dropdown>
-            <Combobox.Options>
-              {options.length === 0 ? (
-                <Combobox.Empty>לא מצאנו משהו כזה</Combobox.Empty>
-              ) : (
-                options
-              )}
-            </Combobox.Options>
-          </Combobox.Dropdown>
-        </Combobox>
-      </Modal>
+          <SearchCombobox
+            events={events}
+            searchValue={searchValue}
+            setSearchValue={setSearchValue}
+          />
+        </Modal>
+      )}
       <section className="w-full py-6 md:py-12 lg:py-18 xl:py-22 px-4 md:px-6 text-white bg-main relative">
         <div className="container mx-auto max-w-3xl text-center">
           <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl mb-4">
@@ -101,22 +134,36 @@ export default function Home() {
         </div>
         <div className="w-full max-w-sm lg:max-w-xl mx-auto space-y-2 absolute bottom-0 left-0 right-0 transform translate-y-1/2 min-w-70">
           <form className="flex center shadow-md" dir="rtl">
-            <input
-              onFocus={(e) => {
-                setShowSearchModal(true);
-                e.target.blur();
-              }}
-              onChange={(e) => setSearchVale(e.target.value)}
-              value={searchVale}
-              placeholder="חפש אירוע..."
-              type="text"
-              className="w-2/3 rounded-r p-2 text-main border"
-            />
+            {!matches ? (
+              <input
+                onFocus={(e) => {
+                  setShowSearchModal(true);
+                  e.target.blur();
+                }}
+                onChange={(e) => setSearchValue(e.target.value)}
+                value={searchValue}
+                placeholder="חפש אירוע..."
+                type="text"
+                className="w-2/3 rounded-r p-2 text-main border"
+              />
+            ) : (
+              <SearchCombobox
+                inline
+                events={events}
+                searchValue={searchValue}
+                setSearchValue={setSearchValue}
+                ref={ref}
+              />
+            )}
             <button
               className="w-1/3 bg-secondary text-main rounded-l"
               onClick={(e) => {
                 e.preventDefault();
-                setShowSearchModal(true);
+                if (!matches) {
+                  setShowSearchModal(true);
+                } else {
+                  ref.current?.focus();
+                }
               }}
             >
               בוא נתחיל לתכנן!
