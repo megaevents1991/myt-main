370725b23b73f1c835e1a93dd373ced364fe0acf
Gilad Leshem <giladlesh@gmail.com>
2025-02-06 19:02:40 +0200

    amenities-icons

---
+++ components/icons/amenitiesIcons.tsx
@@ -0,0 +1,317 @@
+export const icons = [
+  {
+    originalTag: "air-conditioning",
+    displayTag: "AC",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "coffee",
+    displayTag: "coffee/tea",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "fridge",
+    displayTag: "fridge",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "hairdryer",
+    displayTag: "hairdryer",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "heating",
+    displayTag: "heating",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "high-speed-internet-access",
+    displayTag: "wi-fi",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "mini-bar",
+    displayTag: "minibar",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "shower",
+    displayTag: "shower",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "tea-or-coffee",
+    displayTag: "coffee/tea",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "tv",
+    displayTag: "TV",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "wi-fi",
+    displayTag: "wi-fi",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "wired-internet-access",
+    displayTag: "internet",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "with-view",
+    displayTag: "with view",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "24 - hour gym",
+    displayTag: "24h gym",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "24-hour reception",
+    displayTag: "24h reception",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "adults only",
+    displayTag: "adults only",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "all spaces non-smoking (public and private)",
+    displayTag: "non-smoking",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "bar",
+    displayTag: "bar",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "bathtub",
+    displayTag: "bathtub",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "breakfast",
+    displayTag: "breakfast",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "buffet breakfast",
+    displayTag: "buffet breakfast",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "business center",
+    displayTag: "business center",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "casino",
+    displayTag: "casino",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "children swimming pool",
+    displayTag: "kids pool",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "coffee/tea for guests",
+    displayTag: "coffee/tea",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "elevator/lift",
+    displayTag: "elevator",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "fitness facilities",
+    displayTag: "gym",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "flat-screen TV",
+    displayTag: "TV",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "free parking",
+    displayTag: "free parking",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "free wi-fi",
+    displayTag: "wi-fi",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "free internet",
+    displayTag: "wi-fi",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "garden",
+    displayTag: "garden",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "health club",
+    displayTag: "health club",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "heated swimming pool",
+    displayTag: "heated pool",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "in-room internet",
+    displayTag: "in-room wi-fi",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "indoor pool",
+    displayTag: "indoor pool",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "indoor swimming pool",
+    displayTag: "indoor pool",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "internet",
+    displayTag: "wi-fi",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "internet access",
+    displayTag: "wi-fi",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "nightclub",
+    displayTag: "nightclub",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "no elevators",
+    displayTag: "no elevators",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "no parking",
+    displayTag: "no parking",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "no pets allowed",
+    displayTag: "no pets",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "outdoor heated swimming pool",
+    displayTag: "swimming pool",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "outdoor pool",
+    displayTag: "outdoor pool",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "parking",
+    displayTag: "parking",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "poolside bar",
+    displayTag: "poolside bar",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "private beach",
+    displayTag: "private beach",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "reception desk",
+    displayTag: "reception",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "restaurant",
+    displayTag: "restaurant",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "restaurant (buffet style)",
+    displayTag: "buffet restaurant",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "rooftop pool",
+    displayTag: "rooftop pool",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "room service",
+    displayTag: "room service",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "seasonal indoor swimming pool",
+    displayTag: "indoor pool",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "seasonal outdoor swimming pool",
+    displayTag: "outdoor pool",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "smoke-free property",
+    displayTag: "smoke-free",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "smoking allowed",
+    displayTag: "smoking allowed",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "smoking allowed in bedrooms",
+    displayTag: "smoking allowed",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "smoking areas",
+    displayTag: "smoking areas",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "snack bar",
+    displayTag: "snack bar",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "swimming pool",
+    displayTag: "swimming pool",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "terrace",
+    displayTag: "terrace",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+  {
+    originalTag: "wi-fi in the hotel",
+    displayTag: "wi-fi",
+    icon: "noun-air-conditioner-1604106.svg",
+  },
+];
