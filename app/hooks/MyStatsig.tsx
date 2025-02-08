dbdeebde4d4c04c2729f5e16723f5f686f341a88
Yakov Khessin <qvestro@gmail.com>
2025-02-08 22:26:46 +0200

    fix for ios <16.4

--- app/hooks/MyStatsig.tsx
+++ app/hooks/MyStatsig.tsx
@@ -3,7 +3,6 @@
 import React from "react";
 import { StatsigProvider } from "@statsig/react-bindings";
 import { StatsigAutoCapturePlugin } from "@statsig/web-analytics";
-import { StatsigSessionReplayPlugin } from "@statsig/session-replay";
 
 export default function MyStatsig({ children }: { children: React.ReactNode }) {
   return (
@@ -11,10 +10,7 @@ export default function MyStatsig({ children }: { children: React.ReactNode }) {
       sdkKey={process.env.NEXT_PUBLIC_STATSIG_ENV_KEY || ""}
       user={{ userID: "a-user" }}
       options={{
-        plugins: [
-          new StatsigAutoCapturePlugin(),
-          new StatsigSessionReplayPlugin(),
-        ],
+        plugins: [new StatsigAutoCapturePlugin()],
       }}
     >
       {children}
