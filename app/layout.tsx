fe445b1348d9d620035219f7b2c578064a3014e7
Gilad Leshem <giladlesh@gmail.com>
2025-02-25 01:07:40 +0200

    GTM multi env

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,30 +47,36 @@ const theme = createTheme({
   primaryColor: "myColor",
 });
 
+const GTM_TAG = process.env.NEXT_PUBLIC_GTM || undefined;
+const GTM_URL = `https://www.googletagmanager.com/ns.html?id=${GTM_TAG}`;
+
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="he">
-      <head>
-        {/* GTM - Head Script */}
-        <Script id="gtm-head" strategy="afterInteractive">
-          {`
+      {GTM_TAG && (
+        <head>
+          <Script id="gtm-head" strategy="afterInteractive">
+            {`
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TZGBMVJV');
+})(window,document,'script','dataLayer','${GTM_TAG}');
           `}
-        </Script>
-      </head>
+          </Script>
+        </head>
+      )}
       <body className={inter.className}>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-TZGBMVJV"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
+        {GTM_TAG && (
+          <noscript>
+            <iframe
+              src={GTM_URL}
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            ></iframe>
+          </noscript>
+        )}
         <div className="w-screen relative min-h-screen">
           <MixpanelProvider />
           <AuthProvider>
