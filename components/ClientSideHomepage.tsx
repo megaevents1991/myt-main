6edea040c958f3d1646d306bd07dec1643a9beaa
Gilad Leshem <giladlesh@gmail.com>
2025-04-16 15:47:36 +0300

    scroll fix for mobile

--- components/ClientSideHomepage.tsx
+++ components/ClientSideHomepage.tsx
@@ -107,7 +107,15 @@ const SearchCombobox = ({
           onBlur={() => combobox.closeDropdown()}
         />
       </Combobox.Target>
-      <Combobox.Dropdown>
+      <Combobox.Dropdown
+        styles={{
+          dropdown: {
+            maxHeight: "30vh",
+            overflowY: "auto",
+            WebkitOverflowScrolling: "touch",
+          },
+        }}
+      >
         <Combobox.Options>
           {options}
           <Combobox.Option
