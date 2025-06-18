2cd71331dd093c97e4f37d35d57994e50976f029
Gilad Leshem <giladlesh@gmail.com>
2025-06-18 21:34:15 +0300

    removed _clientcues

--- app/layout.tsx
+++ app/layout.tsx
@@ -148,28 +148,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 })(window,document,'script','dataLayer','${GTM_TAG}');
           `}
           </Script>
-          <Script id="clientcues-head" strategy="afterInteractive">
-            {`
-          window['_clientcues_host'] = 'https://www.clientcues.com';
-            window['_clientcues_script'] = "/extensions/clientcues-sr.js";
-            window['_clientcues_namespace'] = 'Cue';
-            window['_clientcues_org'] = 'o01-00000C0Q';
-            (function(w, d, ns, s, src, orgId) {
-                if (!w[ns]) {
-                    var _c = w[ns] = function() {
-                        _c.q ? _c.q.push(arguments) : _c.q = [arguments];
-                    };
-                    _c.q = [];
-                    _c.orgId = orgId; // Save organization ID within the namespace
-                    var el = d.createElement(s),
-                    m = d.getElementsByTagName(s)[0];
-                    el.async = 1;
-                    el.src = w['_clientcues_host'] + src;
-                    m.parentNode.insertBefore(el, m);
-                }
-            })(window, document, window._clientcues_namespace, 'script', window['_clientcues_script'], window['_clientcues_org']);
-          `}
-          </Script>
         </head>
       )}
       <body className={`${inter.className} text-black`}>
