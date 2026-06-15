1f85b6fb85acc9b8c591b2dfb6508ba635d75963
megaevents1991 <megaevents1991@gmail.com>
2026-06-15 16:25:00 +0300

    add category

--- next.config.ts
+++ next.config.ts
@@ -35,6 +35,12 @@ const nextConfig: import("next").NextConfig = {
       },
       {
         protocol: "https",
+        hostname: "fandqafngybfdyslofmr.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/templates/**",
+      },
+      {
+        protocol: "https",
         hostname: "www.avcodes.co.uk",
         port: "",
         pathname: "/images/logos/**",
